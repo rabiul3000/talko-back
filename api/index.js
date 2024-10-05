e916ddc575360423a9af219fc5bc2c4d38dc12b0
rabiul3000 <mdrabiul.asia@gmail.com>
2024-10-05 10:00:17 +0600

    added home route and erro page

--- api/index.js
+++ api/index.js
@@ -9,6 +9,11 @@ app.get('/', (req, res) => {
     res.status(200).send('<h1>Server is running</h1>')
 })
 
+app.get('*', (req, res) => {
+    res.status(200).send('<h1>404 not found</h1>')
+})
+
+
 app.listen(5000, () => {
     console.log('server is running at ' + 5000)
 })
